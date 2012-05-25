919125e3bc70d0d133039df8bacdd06da5875efd
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-05-25 10:48:21 -0400

    Remove the Heroku-appeasing settings.py link (didn't work); Add the DATABASE_URL code to the settings.py file manually.  Remember to also manually add the shared-database:5mb addon.

--- src/project/settings.py
+++ src/project/settings.py
@@ -44,3 +44,50 @@ INSTALLED_APPS = (
 
     'project',
 )
+
+###############################################################################
+#
+# For the sake of Heroku, do this if there is a DATABASE_URL in the
+# environment variables.
+#
+# This is added here so that we can use our own directory structure on Heroku.
+# Remember to manually add the shared-database:5mb addon.
+#
+
+import os
+import sys
+import urlparse
+
+# Register database schemes in URLs.
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+
+try:
+
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print 'Unexpected error:', sys.exc_info()
