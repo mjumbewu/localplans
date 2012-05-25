4b794d63ef8ac4a29091f47c7aed979bc5663857
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-05-25 10:25:24 -0400

    Initial commit of the project.  Includes basic index.html template and configured, simple django project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn_django -b 0.0.0.0:5000 -k gevent --pythonpath src  project.settings
---
+++ src/project/urls.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+)
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
